6eca53a6b6addae7b62dfeac2058144e15b0881b
Nathaniel Parah <nathanielparah579@gmail.com>
2026-04-03 13:19:31 +0100

    added compression methods to the module
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/compress.rs
+++ src/compress.rs
@@ -1,14 +1,19 @@
-use std::{
+use std::{ 
     error::Error,
     path::PathBuf,
     fs::File,
 };
+use zip::{
+    write::SimpleFileOptions,
+    CompressionMethod::Deflated,
+};
+
 
 pub fn zip_compress(file: PathBuf) -> Result<(PathBuf, String), Box<dyn Error>> {
     let file_name = file.file_name()
-                               .ok_or("unable to fetch file name")?
-                               .to_string_lossy()
-                               .to_string();
+       .ok_or("unable to fetch file name")?
+       .to_string_lossy()
+       .to_string();
 
     let parent_dir = file.parent().ok_or("no parent directory found")?;
     let zip_file_name = file_name + ".zip";
@@ -17,6 +22,11 @@ pub fn zip_compress(file: PathBuf) -> Result<(PathBuf, String), Box<dyn Error>> 
     let zip_file = File::create(zip_file_name)?;
 
     let mut zip = zip::ZipWriter::new(zip_file);
+
+    let options = SimpleFileOptions::default()
+        .compression_method(Deflated)
+        .unix_permissions(0o644);
+
 
     Ok((PathBuf::new(), String::from("nat")))
 }
