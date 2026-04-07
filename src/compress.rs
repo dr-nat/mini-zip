f0502b14c33adf27ea88dc88e1b21e38f6a7e291
Nathaniel Parah <nathanielparah579@gmail.com>
2026-04-07 09:55:32 +0100

    made a couple number of changes here
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/compress.rs
+++ src/compress.rs
@@ -8,15 +8,17 @@ use zip::{
     CompressionMethod::Deflated,
 };
 
+use crate::path_to_file::read_file;
+
 
 pub fn zip_compress(file: PathBuf) -> Result<(PathBuf, String), Box<dyn Error>> {
-    let file_name = file.file_name()
+    let file_name = &file.file_name()
        .ok_or("unable to fetch file name")?
        .to_string_lossy()
        .to_string();
 
     let parent_dir = file.parent().ok_or("no parent directory found")?;
-    let zip_file_name = file_name + ".zip";
+    let zip_file_name = file_name.clone() + ".zip";
 
     let new_path = parent_dir.join(&zip_file_name);
     let zip_file = File::create(zip_file_name)?;
@@ -26,6 +28,12 @@ pub fn zip_compress(file: PathBuf) -> Result<(PathBuf, String), Box<dyn Error>> 
     let options = SimpleFileOptions::default()
         .compression_method(Deflated)
         .unix_permissions(0o644);
+    zip.start_file(file_name.clone(), options)?;
+
+    let original_file = read_file(file)?;
+
+    let (mut buffer, buf_file_name) = original_file;
+    let written_file_to_zip = std::io::copy(&mut buffer, &mut zip)?;
 
 
     Ok((PathBuf::new(), String::from("nat")))
