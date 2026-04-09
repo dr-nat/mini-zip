6f4e8e715649297e6ed373b149c77c75b26cf481
Nathaniel Parah <nathanielparah579@gmail.com>
2026-04-09 13:53:04 +0100

    changed the algorithm to accept multiple files at once
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/compress.rs
+++ src/compress.rs
@@ -2,40 +2,48 @@ use std::{
     error::Error,
     path::PathBuf,
     fs::File,
+    io::BufReader,
 };
 use zip::{
     write::SimpleFileOptions,
     CompressionMethod::Deflated,
 };
 
-use crate::path_to_file::read_file;
+//use crate::path_to_file::read_file;
 
+pub fn zip_compress(file: Vec<PathBuf>) -> Result<(), Box<dyn Error>> {
+    for entry in &file {
+        let file_name = &entry.file_name()
+           .ok_or("unable to fetch file name")?
+           .to_string_lossy()
+           .to_string();
 
-pub fn zip_compress(file: PathBuf) -> Result<(PathBuf, String), Box<dyn Error>> {
-    let file_name = &file.file_name()
-       .ok_or("unable to fetch file name")?
-       .to_string_lossy()
-       .to_string();
+        let parent_dir = &entry.parent().ok_or("no parent directory found")?;
+        let zip_file_name = file_name.clone() + ".zip";
 
-    let parent_dir = file.parent().ok_or("no parent directory found")?;
-    let zip_file_name = file_name.clone() + ".zip";
+        let new_path = parent_dir.join(&zip_file_name);
+        let zip_file = File::create(&new_path)?;
 
-    let new_path = parent_dir.join(&zip_file_name);
-    let zip_file = File::create(&new_path)?;
+        let mut zip = zip::ZipWriter::new(zip_file);
 
-    let mut zip = zip::ZipWriter::new(zip_file);
+        let options = SimpleFileOptions::default()
+            .compression_method(Deflated)
+            .unix_permissions(0o644);
+        zip.start_file(file_name.clone(), options)?;
 
-    let options = SimpleFileOptions::default()
-        .compression_method(Deflated)
-        .unix_permissions(0o644);
-    zip.start_file(file_name.clone(), options)?;
+        //let original_file = read_file(file)?;
 
-    let original_file = read_file(file)?;
+        //let (mut buffer, buf_file_name) = original_file;
+        
+        let opened_file = File::open(&entry)?;
+        let mut buffer = BufReader::new(opened_file);
+        let _written_file_to_zip = std::io::copy(&mut buffer, &mut zip)?;
+        zip.finish()?;
 
-    let (mut buffer, buf_file_name) = original_file;
-    let _written_file_to_zip = std::io::copy(&mut buffer, &mut zip)?;
-    zip.finish()?;
+        println!("File Compressed successfully");
+    }
 
-    println!("File Compressed successfully");
-    Ok((new_path, buf_file_name))
+    //let new_path = parent_dir.join(&zip_file_name);
+
+    Ok(())
 }
